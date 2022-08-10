486094d86817f2399a0eabef21187e8a74afdf52
ui-abrarul <abrarulhaquesiddique@cedcommerce.com>
2022-08-10 18:28:00 +0530

    second Commit

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -16,23 +16,54 @@ function Todo() {
 
     const [inputData, setInputData] = useState('')
     const [items, setItems] = useState(getLocalItems())
+    const [toggleSubmit,setToggleSubmit] = useState(true)
+
+    const [isEditItem,setIsEditItem] = useState(null)
 
 
     const addItem = () => {
         if (!inputData) {
+        } else if(inputData && !toggleSubmit) {
+
+            setItems(items.map((elem) => {
+                    if(elem.id === isEditItem){
+                        return {...elem,name: inputData}
+                    }
+                    return elem;
+            }
+            ))
+            setToggleSubmit(true)
+        setInputData('')
+        setIsEditItem(null);
+
 
         } else {
-            setItems([...items, inputData])
+            const allInputData = {
+                id: new Date().getTime().toString(), name: inputData
+            }
+
+            setItems([...items, allInputData])
             setInputData('')
         }
     }
 
     const deleteItem = (id) => {
 
-        const updateditems = items.filter((elem, ind) => {
-            return ind !== id
+        const updateditems = items.filter((elem) => {
+            return elem.id !== id
         })
         setItems(updateditems)
+    }
+
+
+    const editItem = (id) => {
+        let newEditItems = items.find((elem) => {
+            return elem.id === id
+        })
+        console.log(newEditItems)
+        setToggleSubmit(false)
+        setInputData(newEditItems.name)
+        setIsEditItem(id);
     }
 
 
@@ -60,16 +91,27 @@ function Todo() {
                             value={inputData}
                             onChange={(e) => setInputData(e.target.value)}
                         />
-                        <i className="fa fa-plus add-btn" title="Add Item" onClick={addItem}></i>
+                      {toggleSubmit ? <i className="fa fa-plus add-btn" title="Add Item" onClick={addItem}></i>
+                    :
+                    <i className="far fa-edit add-btn" title="Update Item" onClick={addItem}></i>  
+                    
+                    }
                     </div>
 
                     <div className='showItems'>
 
-                        {items.map((elem, ind) => {
+                        {items.map((elem) => {
                             return (
-                                <div className='eachItem' key={ind}>
-                                    <h3>{elem}</h3>
-                                    <i className="far fa-trash-alt add-btn" title="Delete Item" onClick={() => deleteItem(ind)}></i>
+                                <div className='eachItem' key={elem.id}>
+                                    <h3>{elem.name}</h3>
+                                    <div className="todo-btn">
+                                            <i className="far fa-edit add-btn" title="Edit Item" onClick={() => editItem(elem.id)}></i>
+                                        {
+                                            <i className="far fa-trash-alt add-btn" title="Delete Item" onClick={() => deleteItem(elem.id)}></i>
+
+                                        }  
+                                        
+                                        </div>
                                 </div>
                             )
                         })}
